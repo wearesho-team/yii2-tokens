1ad8aa3300b1dadeae2bb96e5be8c2e24d1641e6
Roman Varkuta <roman.varkuta@gmail.com>
2019-01-17 17:47:09 +0200

    Add test for Repository (#4)

---
+++ tests/Unit/RepositoryTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Wearesho\Yii2\Token\Tests\Unit;
+
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+use yii\redis\Connection;
+use yii\di\Container;
+use Wearesho\Yii2\Token;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Class RepositoryTest
+ * @package Wearesho\Yii2\Token\Tests\Unit
+ */
+class RepositoryTest extends TestCase
+{
+    public function testPut(): void
+    {
+        Carbon::setTestNow(Carbon::make('2010-01-01'));
+        $expireAt = Carbon::make('2020-01-01');
+        $expireDiff = Carbon::getTestNow()->diffInSeconds($expireAt);
+        $hash = Uuid::uuid4();
+        \Yii::$container = new Container();
+        \Yii::$container->set(
+            Token\Repository::class,
+            function ($container, $params, $config) use ($hash, $expireDiff) {
+                $stub = $this->createMock(Connection::class);
+
+                $map = [
+                    [
+                        'hset',
+                        ["y2rt-{$hash}", 'type'],
+                    ],
+                    [
+                        'hset',
+                        ["y2rt-{$hash}", 'owner'],
+                    ],
+                    [
+                        'hset',
+                        ["y2rt-{$hash}", 'value'],
+                    ],
+                ];
+
+                $stub->expects($this->at(0))
+                    ->method('__call')
+                    ->with(
+                        'multi'
+                    )
+                    ->willReturn($this->returnValue(null));
+
+                $stub->expects($this->at(1))
+                    ->method('__call')
+                    ->willReturn(
+                        $this->returnValueMap($map)
+                    );
+
+                $stub->expects($this->at(5))
+                    ->method('__call')
+                    ->with(
+                        $this->equalTo('exec')
+                    )
+                    ->willReturn(
+                        $this->returnValue(
+                            $this->equalTo(["y2rt-{$hash}", $expireDiff])
+                        )
+                    );
+
+                return new Token\Repository([
+                    'redis' => $stub
+                ]);
+            }
+        );
+
+        /** @var Token\Repository $repository */
+        $repository = \Yii::$container->get(Token\Repository::class);
+
+        $this->assertTrue(
+            Uuid::isValid($repository->put(new Token\Entity('type', 'owner', 'value', $expireAt)))
+        );
+        Carbon::setTestNow();
+    }
+}
