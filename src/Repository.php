b72c72cfcfbf90abec1f1bdbe5e7da8c9a48278b
Alexander Letnikow <reclamme@gmail.com>
2019-01-17 14:08:30 +0200

    Add Repository to store tokens in redis

---
+++ src/Repository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Wearesho\Yii2\Token;
+
+use yii\redis;
+use Carbon\Carbon;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Class Repository
+ * @package Wearesho\Yii2\Token
+ */
+class Repository
+{
+    protected const FIELD_TYPE = 'type';
+    protected const FIELD_OWNER = 'owner';
+    protected const FIELD_VALUE = 'value';
+
+    /** @var string|array|redis\Connection */
+    public $redis = 'redis';
+
+    public function put(EntityInterface $token): string
+    {
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $hash = Uuid::uuid4();
+        $key = $this->getKey($hash);
+        $this->redis->multi();
+
+        $this->redis->hset($key, static::FIELD_TYPE, $token->getType());
+        $this->redis->hset($key, static::FIELD_OWNER, $token->getOwner());
+        $this->redis->hset($key, static::FIELD_VALUE, $token->getValue());
+
+        $expire = Carbon::now()->diffInSeconds($token->getExpireAt());
+        $this->redis->expire($key, $expire);
+
+        $this->redis->exec();
+
+        return $hash;
+    }
+
+    public function get(string $hash): ?EntityInterface
+    {
+        if (!$this->validate($hash)) {
+            return null;
+        }
+
+        $key = $this->getKey($hash);
+        if (!$this->redis->hexists($key, static::FIELD_TYPE)) {
+            return null;
+        }
+
+        $type = $this->redis->hget($key, static::FIELD_TYPE);
+        $owner = $this->redis->hget($key, static::FIELD_OWNER);
+        $value = $this->redis->hget($key, static::FIELD_VALUE);
+        $expireAt = Carbon::now()->addSeconds($this->redis->ttl($key));
+
+        return new Entity($type, $owner, $value, $expireAt);
+    }
+
+    protected function getKey(string $hash): string
+    {
+        return "y2rt-{$hash}";
+    }
+
+    protected function validate(string $hash): bool
+    {
+        return Uuid::isValid($hash);
+    }
+}
